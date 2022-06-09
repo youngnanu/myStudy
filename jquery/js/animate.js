3359b89a66586e72611d94d5e8d83647276bcc8d
kim young uk <nsns4993@gmail.com>
2022-06-10 00:12:46 +0900

    jquery ui 추가

--- jquery/js/animate.js
+++ jquery/js/animate.js
@@ -1,6 +1,13 @@
 $(function(){
   $('#typo .inner').click(function(){
-    //선택자.animate({속성:값,속성:값}, 시간, 이징, 다른할일);
-    $(this).animate({opacity:0, fontSize:'0px'},1000);
+    //선택자.animate({속성:값,속성:값}, 시간, 이징, 끝나고난뒤다른할일);
+    $(this).animate({opacity:0, fontSize:'0px'},1000, 'easeInOutElastic', 
+      function(){
+        $(this).animate({opacity:1,fontSize:'110px'},500);
+      }
+    );
   });
+  $('ul').on('click', 'li', function(){
+    $(this).animate({backgroundColor:'#ccc', color:'#ff0000'}, 1000);
+  })
 });
